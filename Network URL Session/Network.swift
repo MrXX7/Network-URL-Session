4c4252537092e31566b1f6c535c8614c72fc737d
Oncu <oohancan@gmail.com>
2023-10-20 00:02:27 +0300

    Added network request for fetching users from API

---
+++ Network URL Session/Network.swift
@@ -0,0 +1,18 @@
+//
+//  Network.swift
+//  Network URL Session
+//
+//  Created by Oncu Can on 19.10.2023.
+//
+
+import SwiftUI
+
+struct Network: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Network()
+}
---
+++ Network URL Session/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Network URL Session
+//
+//  Created by Oncu Can on 19.10.2023.
+//
+
+import Foundation
