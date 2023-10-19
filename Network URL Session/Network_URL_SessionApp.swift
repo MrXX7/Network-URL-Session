a85afcdbb120be1aca4bc247a3f5b33d48af0cd8
Oncu <oohancan@gmail.com>
2023-10-19 23:09:12 +0300

    Initial Commit

---
+++ Network URL Session/Network_URL_SessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_URL_SessionApp.swift
+//  Network URL Session
+//
+//  Created by Oncu Can on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Network_URL_SessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
